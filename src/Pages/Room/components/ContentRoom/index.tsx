afd5ec61f8425ac05b475c8b8fb1c5314c6dbb03
caioRafael <caiorafaelrg@gmail.com>
2022-12-03 17:51:26 -0300

    lista de usuários na sala

--- src/Pages/Room/components/ContentRoom/index.tsx
+++ src/Pages/Room/components/ContentRoom/index.tsx
@@ -1,11 +1,22 @@
 import { Content } from "./styles";
 import { HeaderContent, MainContent, FooterContent} from "../";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import { database } from "../../../../service/firebase";
+import { onValue, ref } from "firebase/database";
+import { useRoom } from "../../../../hooks/useRoom";
 
+/**
+ * 1- criar uma função para toda a sala, em que ira trazer os dados em tempo real
+ * qunado os dados forem revelados, utilizar a função onValue
+ * 2- listar os usuários para com votos de forma separada
+ */
  
 const ContentRoom = () => {
+    const {code} = useParams()
+    const {room, users} = useRoom(code as string)
     const [list, setList] = useState<number[]>([])
-    const [vote, setVode] = useState<string>('')
+
     return (
         <Content>
             <HeaderContent setList={setList}/>
