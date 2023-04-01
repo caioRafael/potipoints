877a3e7c28ec2fb1817f20c933f3880412b0fed3
caioRafael <caiorafaelrg@gmail.com>
2023-04-01 18:23:26 -0300

    version

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { ThemeProvider } from 'styled-components'
 import Routes from './routes'
 import { Global } from './styles/global'
 import { defaultTheme } from './styles/themes/defaultTheme'
+import { version } from '../package.json'
+import { VersionFooter } from './components'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <div className="App">
+        <VersionFooter>V{version}</VersionFooter>
         <Global />
         <Routes />
         <ToastContainer />
---
+++ src/components/VersionFooter.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const VersionFooter = styled.div`
+  position: absolute;
+  bottom: 8px;
+  right: 10px;
+  color: ${(props) => props.theme.colors.primary};
+`
